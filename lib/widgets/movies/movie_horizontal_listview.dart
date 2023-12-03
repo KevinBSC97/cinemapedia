1663b71a7ecd93767092bcfb2d23e68e9ded5e20
KevinBSC97 <kevinsotomayor1@hotmail.com>
2023-12-02 21:04:22 -0500

    se modifico el acceso a la pelicula individual

--- lib/widgets/movies/movie_horizontal_listview.dart
+++ lib/widgets/movies/movie_horizontal_listview.dart
@@ -101,7 +101,8 @@ class _Slide extends StatelessWidget {
                     );
                   }
                   return GestureDetector(
-                    onTap: () => context.push('/movie/${movie.id}'),
+                    //onTap: () => context.push('/movie/${movie.id}'),
+                    onTap: () => context.push('/home/0/movie/${movie.id}'),
                     child: FadeIn(child: child),
                   );
                 },
